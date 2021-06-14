950d776866880a1f97f87f203cd4c2ee022a96f7
FelipeLucasRoll <57493030+FelipeLucasRoll@users.noreply.github.com>
2021-06-14 20:11:42 -0300

    Update Main.java
    Mensagens arrumadas

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,8 @@ public class Main {
 
         l.le(arquivo);
 
-        System.out.println("1. Particoes Fixas");
-        System.out.println("2. Particoes Variaveis");
+        System.out.println("1. Partições Fixas");
+        System.out.println("2. Partições Variáveis ");
         System.out.println("0. Sair");
 
         int op = scan.nextInt();
@@ -25,27 +25,27 @@ public class Main {
     }
 
     public static void menuFixo() {
-        System.out.println("Tamanho da memoria");
+        System.out.println("Tamanho da memória ");
         int tamanhoDaMemoria = scan.nextInt();
 
-        System.out.println("Tamanho das particoes");
+        System.out.println("Tamanho das partições");
         int tamanhoDaParticao = scan.nextInt();
         
         tamanhoDaMemoria = (int) Math.pow(2, tamanhoDaMemoria);
         tamanhoDaParticao = (int) Math.pow(2, tamanhoDaParticao);
 
         if(tamanhoDaMemoria / tamanhoDaParticao < 1) { 
-            System.out.println("O tamanho de particao nao pode ser maior que a memoria");
-            menuFixo();
+            System.out.println("O tamanho de partição  não pode ser maior que a memória");
+            menuFixo()
         } else {
             sim = new Simulador(tamanhoDaMemoria, tamanhoDaParticao);
         }
     }
 
     public static void menuVariavel() {
-        System.out.println("Tamanho da memoria");
+        System.out.println("Tamanho da memória");
         int tamanhoDaMemoria = scan.nextInt();
-        System.out.println("Politica de alocacao");
+        System.out.println("Política de alocação");
         System.out.println("1. Best-Fit");
         System.out.println("2. Worst-FIt");
         int op = scan.nextInt();
