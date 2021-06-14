fb70f4c9a1d7aab5598310d47f7d5b88747ba18f
FelipeLucasRoll <57493030+FelipeLucasRoll@users.noreply.github.com>
2021-06-14 20:15:47 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -35,7 +35,7 @@ public class Main {
         tamanhoDaParticao = (int) Math.pow(2, tamanhoDaParticao);
 
         if(tamanhoDaMemoria / tamanhoDaParticao < 1) { 
-            System.out.println("O tamanho de partição  não pode ser maior que a memória");
+            System.out.println("O tamanho de partição não pode ser maior que a memória");
             menuFixo()
         } else {
             sim = new Simulador(tamanhoDaMemoria, tamanhoDaParticao);
@@ -47,7 +47,7 @@ public class Main {
         int tamanhoDaMemoria = scan.nextInt();
         System.out.println("Política de alocação");
         System.out.println("1. Best-Fit");
-        System.out.println("2. Worst-FIt");
+        System.out.println("2. Worst-Fit");
         int op = scan.nextInt();
         if(op == 1) { sim = new Simulador((int) Math.pow(2, tamanhoDaMemoria), new BestFit()); }
         if(op == 2) { sim = new Simulador((int) Math.pow(2, tamanhoDaMemoria), new WorstFit()); }
